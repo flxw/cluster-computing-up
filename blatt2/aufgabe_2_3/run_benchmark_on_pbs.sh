f3d79feada1b02127502567c5ec929bf30f55619
Felix Wolff <felix.wolff2@student.hpi.de>
2017-05-30 23:43:12 +0200

    Fix Makefile for Zuse and PBS

--- blatt2/aufgabe_2_3/run_benchmark_on_pbs.sh
+++ blatt2/aufgabe_2_3/run_benchmark_on_pbs.sh
@@ -3,25 +3,19 @@
 # extra node hostnames from stupid PBS_NODEFILE
 IFS=$'\r\n' GLOBIGNORE='*' command eval  'NODEARRAY=($(sort -u $PBS_NODEFILE))'
 COMPUTENODE="${NODEARRAY[0]}"
+echo "Running the whole thing on $COMPUTENODE"
 
-# copy executables over
-for EXE in "montecarlo_pi" "affine_montecarlo_pi" "benchmark.sh" "test_consistency"; do
-	scp "$EXE" "$COMPUTENODE:"
-done
+# PBS script gets executed in node home directory, even on master node
+# also the home directory is copied everywhere, so there is no need to copy binary files
 
 # run script on node
 ssh "$COMPUTENODE" 'bash -s' << 'ENDSSH'
+cd ~/cluster-computing-up/blatt2/aufgabe_2_3
 for EXE in "montecarlo_pi" "affine_montecarlo_pi"; do
-	./benchmark.sh "./$EXE" 1000000000 16 benchmark_results_$EXE.txt
+	./benchmark.sh "./$EXE" 10000000 16 benchmark_results_$EXE.txt
 
-	for $SAMPLESIZE in 10000 100000 1000000 10000000 100000000 1000000000; do
-		#./test_consistency "./$EXE" "$SAMPLESIZE" 8 100 consistency_results_$EXE.txt
+	for SAMPLESIZE in 10000 100000 1000000 10000000; do
+		./test_consistency.sh "./$EXE" "$SAMPLESIZE" 8 100 consistency_results_$EXE.txt
 	done
 done
 ENDSSH
-
-# copy results back from computenode
-for EXE in "montecarlo_pi" "affine_montecarlo_pi"; do
-	scp "$COMPUTENODE:benchmark_results_$EXE.txt"   .
-	scp "$COMPUTENODE:consistency_results_$EXE.txt" .
-done
