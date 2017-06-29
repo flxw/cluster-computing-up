0a1b8dd0e9c29f30a4927277dfa6a05663be5fb6
Felix Wolff <felix.wolff2@student.hpi.de>
2017-06-29 14:58:30 +0200

    Add script to run the thing on the cluster

---
+++ blatt4/aufgabe_4_3/run_benchmark.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# extra node hostnames from stupid PBS_NODEFILE
+IFS=$'\r\n' GLOBIGNORE='*' command eval  'NODEARRAY=($(sort -u $PBS_NODEFILE))'
+TARGETS=(procon omp_sec_procon omp_parallel_procon)
+
+# PBS script gets executed in node home directory, even on master node
+# also the home directory is copied everywhere, so there is no need to copy binary files
+for NODE in "${NODEARRAY[@]}"; do
+ssh "$NODE" 'bash -s' << 'ENDSSH'
+  cd   ~/cluster-computing-up/blatt4/aufgabe_4_3
+  
+  echo procon
+  /usr/bin/time -p ./procon
+  echo omp_sec_procon
+  /usr/bin/time -p ./omp_sec_procon
+  echo omp_parallel_procon
+  /usr/bin/time -p ./omp_parallel_procon
+
+ENDSSH
+done
